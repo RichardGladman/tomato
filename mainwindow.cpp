4733bd936d7862409eeb4f15c4c6bb7171ba446d
Richard Gladman <github@thefifthcontinent.com>
2026-05-03 04:29:07 +0100

    Layout window

---
+++ mainwindow.cpp
@@ -0,0 +1,46 @@
+#include "mainwindow.h"
+
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+#include <QLabel>
+#include <QPushButton>
+#include <QTimer>
+#include <QMediaPlayer>
+#include <QSoundEffect>
+#include <QTimer>
+#include <QPixmap>
+#include <qboxlayout.h>
+#include <qfont.h>
+#include <qpushbutton.h>
+
+MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent)
+{
+	setWindowTitle("Tomato");
+	resize(400, 180);
+	
+	QWidget *centralWidget = new QWidget(this);
+	setCentralWidget(centralWidget);
+	
+	QHBoxLayout *mainLayout = new QHBoxLayout(centralWidget);
+	QVBoxLayout *rightLayout = new QVBoxLayout(centralWidget);
+	
+	QPixmap pixmap = QPixmap("tomato.png");
+	//Q_ASSERT(!pixmap.isNull());
+	QLabel *logo = new QLabel(centralWidget);
+	logo->setPixmap(pixmap);
+	
+	QFont font = QFont("Arial", 20);
+	QLabel *message = new QLabel("Working...", centralWidget);
+	message->setFont(font);
+	
+	QPushButton *settingsButton = new QPushButton("Settings", this);
+	
+	mainLayout->addWidget(logo);
+	mainLayout->addLayout(rightLayout);
+	rightLayout->addWidget(message);
+	rightLayout->addWidget(settingsButton);
+	this->setLayout(mainLayout);
+	
+}
+
+MainWindow::~MainWindow() = default;
